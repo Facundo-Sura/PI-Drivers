f07ae79659e29098377a648a0a3c26d6fdd00cc6
Facundo-Sura <facundomesura@gmail.com>
2024-04-04 06:42:44 -0300

    PI-Drivers-main

---
+++ server/src/models/Driver.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define(
+    "Driver",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      forname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      surname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      nationality: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      dob: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    { timetamps: false }
+  );
+};
