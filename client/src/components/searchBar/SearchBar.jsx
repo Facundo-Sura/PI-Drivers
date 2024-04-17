60c2efebb02b7bd1547cd7b6461d24c3b0bb39d1
Facundo Sura <facundomesura@gmail.com>
2024-04-17 00:57:09 -0300

    home Styles

--- client/src/components/searchBar/SearchBar.jsx
+++ client/src/components/searchBar/SearchBar.jsx
@@ -1,34 +1,41 @@
-import { Link } from "react-router-dom"
-import useSearch from "../../hooks/useSearch"
-import style from "./SearchBar.module.css"
-import { useSelector } from "react-redux"
-import filter from "../../hooks/filter"
+import { Link } from "react-router-dom";
+import useSearch from "../../hooks/useSearch";
+import style from "./SearchBar.module.css";
+import { useSelector } from "react-redux";
+import filter from "../../hooks/filter";
 const SearchBar = () => {
-    
-    const {input,setInput,handleSearch} = useSearch()
-    const aux = useSelector(state=> state.aux)
-    const handleChange = (event) => {
-        setInput(event.target.value)
-    }
-    const handleClick = (event) => {
-        event.preventDefault();
-        handleSearch()
-    }
+  const { input, setInput, handleSearch } = useSearch();
+  const aux = useSelector((state) => state.aux);
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+  const handleClick = (event) => {
+    event.preventDefault();
+    handleSearch();
+  };
 
-    return(
-        <>
-        <div className={style.search}>
-            <div>
-                <button onClick={()=>filter(aux)}>nationality</button>
-                <Link to="/formPage">
-                <button className={style.button}>Create new!</button>
-                </Link>
-            </div> 
-            <input type="text" className={style.input} value={input} onChange={handleChange} />
-            <button className={style.searchbutton} onClick={handleClick}>Search</button>
+  return (
+    <>
+      <div className={style.search}>
+        <div className={style.createnew}>
+          <Link to="/formPage">
+            <button className={style.button}>Create new</button>
+          </Link>
         </div>
-        </>
-    )
-}
+        <div className={style.searchbar}>
+          <input
+            type="text"
+            className={style.input}
+            value={input}
+            onChange={handleChange}
+          />
+          <button className={style.searchbutton} onClick={handleClick}>
+            <img src="/src/imagenes/i3kvlpusd9rv8diq849o468brk.png" />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
