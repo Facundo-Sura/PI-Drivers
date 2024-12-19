cced364f6e86997742fce88566072fc75e1da870
Facundo Sura <facundomesura@gmail.com>
2024-12-19 11:00:34 -0300

    minimos cambios en el detail

--- client/src/views/detail/Detail.jsx
+++ client/src/views/detail/Detail.jsx
@@ -1,13 +1,12 @@
-import { useEffect, useState } from "react";
 import style from "./Detail.module.css";
 import axios from "axios";
+import { useEffect, useState } from "react";
 import { URL_BASE } from "../../endpoints";
 import { useParams } from "react-router-dom";
 import Navbar from "../../components/navbar/Navbar";
 import Footer from "../../components/footer/Footer";
 
 const detailPage = () => {
-  // const {name,lastName,nationality,image,description,dob,teams} = driverDetail;
   const [state, setState] = useState({});
   const { name, lastName, nationality, image, description, dob, teams } = state;
   const { id } = useParams();
@@ -36,7 +35,7 @@ const detailPage = () => {
               Teams:{" "}
               {teams?.map((team) => {
                 return (
-                  <span key={team} volue={team} className={style.pyh}>
+                  <span className={style.pyh} key={team} volue={team}>
                     {team}
                     {". "}
                   </span>
